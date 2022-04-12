81ac5f65398f7f7b6b8aee9a740dcefdf2be6202
Aman Sharma <shrma.aman7@gmail.com>
2022-04-12 15:41:58 +0530

    Initial Commit

---
+++ FRUCTUS/FRUCTUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUCTUSApp.swift
+//  FRUCTUS
+//
+//  Created by AMAN SHARMA on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FRUCTUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
